dd748786d196e3e0e06200ee0cecddb80834139e
Sabrina <sabrinalk22@gmail.com>
2026-03-17 16:26:44 +0000

    the basiscs

--- 690Project/Project/ConsoleUI.cs
+++ 690Project/Project/ConsoleUI.cs
@@ -1,28 +1,91 @@
+using System.Reflection.Emit;
+using Microsoft.VisualBasic;
+using Spectre.Console;
+
 namespace Project;
 public class ConsoleUI
     {
         FileSaver fileSaver;
+
+        List<Category> categories; 
+        List<Label> labels; 
+
+        List<User> users; 
+
         public ConsoleUI()
         {
             fileSaver = new FileSaver("task-data.txt");
+
+            categories = new List<Category>(); 
+            categories.Add(new Category("Food"));
+            categories.Add(new Category("Vet"));
+            categories.Add(new Category("3"));
+
+            labels = new List<Label>();
+            labels.Add(new Label("treats"));
+            labels.Add(new Label("kibble"));
+            labels.Add(new Label("vaccines"));
+            labels.Add(new Label("check up"));
+
+            // add different labels to different categories 
+            categories[0].Labels.Add(labels[0]);
+            categories[0].Labels.Add(labels[1]);
+            categories[1].Labels.Add(labels[2]);
+            categories[1].Labels.Add(labels[3]);
+
+            users = new List<User>();
+            users.Add(new User("Jane"));
+            users.Add(new User("John"));
         }
         public void Show(){   
-            string user = AskForInput("Please select mode new user or current user: ");
+            var user = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Please select mode new user or current user: ")
+                    .AddChoices("new user", "current user"));
 
             if(user == "current user"){
 
              string command = ""; 
 
                 do {
-                    string task = AskForInput("Please select upcoming tasks or current tasks:");
+                    var selectedUser = AnsiConsole.Prompt(
+                    new SelectionPrompt<User>()
+                        .Title("Pleaser select a user:")
+                        .AddChoices(users));
+                    Console.WriteLine("Current user is: " + selectedUser.Name);
+
+                    var selectedInput = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Please select upcoming tasks or current tasks:")
+                        .AddChoices("incomplete", "complete"));
+                    Status selectedStatus = new Status(selectedInput == "complete");
+
+
+                    var viewEdit = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Please select: view tasks or edit tasks:")
+                            .AddChoices("view tasks", "edit tasks"));
+                     
+                    var selectedCategory = AnsiConsole.Prompt(
+                        new SelectionPrompt<Category>()
+                        .Title("Please select task category: ")
+                        .AddChoices(categories));
+
+                    var selectedLabel = AnsiConsole.Prompt(
+                        new SelectionPrompt<Label>()
+                        .Title("Please select task label: ")
+                        .AddChoices(selectedCategory.Labels));
                     
-                    string viewEdit = AskForInput("Please select: view tasks or edit tasks:");
-                    
-                    string taskCategory = AskForInput("Please select task category:");
-                    
-                    string dateTime = AskForInput("Please enter upcoming task due date and time:");
+                    string selectedDate = AskForInput("Please enter upcoming task due date and time:");
+                    DateTime dueDate;
+                    while (!DateTime.TryParse(selectedDate, out dueDate))
+                        {
+                            selectedDate = AskForInput("Invalid format. Please enter a valid date and time (e.g., 2026-03-17 14:30):");
+                        }
+
+                    TaskData data = new TaskData(dueDate, selectedUser, selectedCategory, selectedLabel, selectedStatus);
             
-                    fileSaver.AppendLine(dateTime + "\n");
+                    fileSaver.AppendData(data);
 
                     command = AskForInput("Enter submit"); 
                 } while (command != "submit");   
---
+++ 690Project/Project/Domain.cs
@@ -0,0 +1,77 @@
+
+using Microsoft.VisualBasic;
+
+namespace Project;
+
+public class User {
+    public string Name { get; }
+
+    public User(string name) {
+        this.Name = name;
+    }
+
+     public override string ToString()
+    {
+        return this.Name;
+    }
+    
+}
+
+public class Label
+{
+    public string Name { get; }
+
+    public Label(string name) {
+        this.Name = name;
+    }
+
+    public override string ToString()
+    {
+        return this.Name;
+    }
+    
+}
+
+public class Category
+{
+    public string Name { get; }
+    public List<Label> Labels { get; }
+    public Category (string name) {
+        this.Name = name;
+        this.Labels = new List<Label>();
+    }
+    public override string ToString()
+    {
+        return this.Name;
+    }
+}
+
+
+
+public class Status
+{
+    public bool Complete { get; set; }
+    public Status(bool complete) {
+        this.Complete = complete;
+    }
+    public override string ToString(){
+        return Complete ? "complete" : "incomplete";
+    }
+}
+
+public class TaskData
+{
+    public DateTime DueDate { get; }
+    public User User { get; }
+    public Category Category { get; }
+    public Label Label { get; }
+    public Status Status { get; }
+    public TaskData(DateTime dueDate, User user, Category category, Label label, Status status) {
+        this.DueDate = dueDate;
+        this.User = user;
+        this.Category = category;
+        this.Label = label; 
+        this.Status = status;
+    }
+}
+
