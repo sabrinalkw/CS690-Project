b8bcd1d481465622659b7e83aa3ebbb2d871d5eb
Sabrina <sabrinalk22@gmail.com>
2026-03-17 19:37:50 +0000

    persistant storage

--- 690Project/Project/FileSaver.cs
+++ 690Project/Project/FileSaver.cs
@@ -7,7 +7,9 @@ public class FileSaver
     string fileName;
     public FileSaver(string fileName) {
         this.fileName = fileName;
+        if(!File.Exists(this.fileName)){
         File.Create(this.fileName).Close();
+        }
     }
 
     public void AppendLine(string line)
