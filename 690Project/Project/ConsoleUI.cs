e719fb867939cbbdd63a1d2bcad929486a685c92
Sabrina <sabrinalk22@gmail.com>
2026-03-25 20:29:08 +0000

    FR 5a and FR1a  (half done )and linting(charpier)

--- 690Project/Project/ConsoleUI.cs
+++ 690Project/Project/ConsoleUI.cs
@@ -1,160 +1,254 @@
+using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Linq.Expressions;
 using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using Microsoft.VisualBasic;
 using Spectre.Console;
 
 namespace Project;
+
 public class ConsoleUI
-    { 
-        DataManager dataManager;
-        public ConsoleUI()
-        {
-            
-            dataManager = new DataManager();
-        }
-        public void Show()
-    {   
-    var user = AnsiConsole.Prompt(
-        new SelectionPrompt<string>()
-            .Title("Please select mode new user or current user: ")
-            .AddChoices("new user", "current user"));
-        
+{
+    DataManager dataManager;
 
-    if (user == "current user")
+    public ConsoleUI()
     {
-        string command = ""; 
+        dataManager = new DataManager();
+    }
 
-        do
+    public void Show()
+    {
+        var user = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Please select mode new user or current user: ")
+                .AddChoices("new user", "current user")
+        );
+
+        if (user == "current user")
         {
-            var selectedUser = AnsiConsole.Prompt(
-                new SelectionPrompt<User>()
-                    .Title("Pleaser select a user:")
-                    .AddChoices(dataManager.Users));
+            string command = "";
 
-            Console.WriteLine("Current user is: " + selectedUser.Name);
-
-            var selectedStatus = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("Please select upcoming tasks or current tasks:")
-                    .AddChoices("upcoming", "completed"));
-
-            Status taskStatus = new Status(selectedStatus == "completed");
-
-            if (selectedStatus == "completed")
+            do
             {
-                var viewEdit = AnsiConsole.Prompt(
+                var selectedUser = AnsiConsole.Prompt(
+                    new SelectionPrompt<User>()
+                        .Title("Pleaser select a user:")
+                        .AddChoices(dataManager.Users)
+                );
+
+                Console.WriteLine("Current user is: " + selectedUser.Name);
+
+                var selectedStatus = AnsiConsole.Prompt(
                     new SelectionPrompt<string>()
-                        .Title("Please select: view tasks or edit tasks:")
-                        .AddChoices("view tasks", "edit tasks"));
+                        .Title("Please select upcoming tasks or current tasks:")
+                        .AddChoices("upcoming", "completed")
+                );
 
-                if(viewEdit == "edit tasks") 
+                Status taskStatus = new Status(selectedStatus == "completed");
+
+                if (selectedStatus == "completed")
                 {
-                var updateEdit = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("Mark previously entered task as complete or add new task ")
-                    .AddChoices("update previously entered task", "add new task"));
+                    var viewEdit = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Please select: view tasks or edit tasks:")
+                            .AddChoices("view tasks", "edit tasks")
+                    );
 
-                if (updateEdit == "add new task")
-                        {
-                            var selectedCategory = AnsiConsole.Prompt(
-                    new SelectionPrompt<Category>()
-                        .Title("Please select task category: ")
-                        .AddChoices(dataManager.Categories));
-
-                var selectedLabel = AnsiConsole.Prompt(
-                    new SelectionPrompt<Label>()
-                        .Title("Please select task label: ")
-                        .AddChoices(selectedCategory.Labels));
-
-                var dueDate = AnsiConsole.Prompt(
-                    new TextPrompt<DateTime>("Please enter upcoming task due date and time (in m/d/y hr:min format):"));
-
-                TaskData data = new TaskData(
-                    dueDate, selectedUser, selectedCategory, selectedLabel, taskStatus);
-        
-                dataManager.AddNewTaskData(data);
-
-                command = AskForInput("Enter submit: ");
-                } 
-        
-                else if (updateEdit == "update previously entered task")
+                    if (viewEdit == "edit tasks")
                     {
-                    var incompleteTasks = Reporter.ShowTasksUpcoming(dataManager.TaskData).ToList();
-                              
-                    var selectedUpdate = AnsiConsole.Prompt(
-                    new SelectionPrompt<TaskData>()
-                        .Title("Please select task to mark as complete ")
-                        .AddChoices(incompleteTasks));
+                        var updateEdit = AnsiConsole.Prompt(
+                            new SelectionPrompt<string>()
+                                .Title("Mark previously entered task as complete or add new task ")
+                                .AddChoices("update previously entered task", "add new task")
+                        );
 
-                       selectedUpdate.Status.Complete = true;
-                       dataManager.SaveAllTasks();
+                        if (updateEdit == "add new task")
+                        {
+                            string listUpdate = AnsiConsole.Prompt(
+                                new SelectionPrompt<string>()
+                                    .Title(
+                                        "Please select: choose from list or add new task category"
+                                    )
+                                    .AddChoices(
+                                        "add new task or category",
+                                        "choose from existing list"
+                                    )
+                            );
+
+                            if (listUpdate == "choose from existing list")
+                            {
+                                var selectedCategory = AnsiConsole.Prompt(
+                                    new SelectionPrompt<Category>()
+                                        .Title("Please select task category: ")
+                                        .AddChoices(dataManager.Categories)
+                                );
+
+                                var selectedLabel = AnsiConsole.Prompt(
+                                    new SelectionPrompt<Label>()
+                                        .Title("Please select task label: ")
+                                        .AddChoices(selectedCategory.Labels)
+                                );
+
+                                var dueDate = AnsiConsole.Prompt(
+                                    new TextPrompt<DateTime>(
+                                        "Please enter upcoming task due date and time (in m/d/y hr:min format):"
+                                    )
+                                );
+
+                                TaskData data = new TaskData(
+                                    dueDate,
+                                    selectedUser,
+                                    selectedCategory,
+                                    selectedLabel,
+                                    taskStatus
+                                );
+
+                                dataManager.AddNewTaskData(data);
+
+                                command = AskForInput("Enter submit: ");
+                            }
+                            else if (listUpdate == "add new task or category")
+                            {
+                                var txtUpdate = AnsiConsole.Prompt(
+                                    new SelectionPrompt<string>()
+                                        .Title(
+                                            "Please select: add new category or add new task for existing category"
+                                        )
+                                        .AddChoices(
+                                            "add new category",
+                                            "add new task for existing category"
+                                        )
+                                );
+
+                                if (txtUpdate == "add new category")
+                                {
+                                    var newCategoryName = AnsiConsole.Prompt(
+                                        new TextPrompt<string>("Enter new category name:")
+                                    );
+                                    dataManager.AddCategory(new Category(newCategoryName));
+
+                                    var newLabelName = AnsiConsole.Prompt(
+                                        new TextPrompt<string>("Enter new task for this category:")
+                                    );
+                                    var addedCategory = dataManager.Categories.Last();
+                                    dataManager.AddLabel(new Label(newLabelName), addedCategory);
+                                }
+                                else if (txtUpdate == "add new task for existing category")
+                                {
+                                    var selectedCategory = AnsiConsole.Prompt(
+                                        new SelectionPrompt<Category>()
+                                            .Title("Please select task category: ")
+                                            .AddChoices(dataManager.Categories)
+                                    );
+
+                                    var newLabelName = AnsiConsole.Prompt(
+                                        new TextPrompt<string>("Enter new task for this category:")
+                                    );
+                                    dataManager.AddLabel(new Label(newLabelName), selectedCategory);
+                                }
+                            }
+                        }
+                        else if (updateEdit == "update previously entered task")
+                        {
+                            var incompleteTasks = Reporter
+                                .ShowTasksUpcoming(dataManager.TaskData)
+                                .ToList();
+
+                            var selectedUpdate = AnsiConsole.Prompt(
+                                new SelectionPrompt<TaskData>()
+                                    .Title("Please select task to mark as complete ")
+                                    .AddChoices(incompleteTasks)
+                            );
+
+                            selectedUpdate.Status.Complete = true;
+                            dataManager.SaveAllTasks();
+                        }
+                    }
+
+                    if (viewEdit == "view tasks")
+                    {
+                        var result = Reporter.ShowTasksCompleted(dataManager.TaskData);
+                        Console.WriteLine("Your completed tasks are:");
+                        foreach (var task in result)
+                        {
+                            Console.WriteLine(
+                                $"- User: {task.User} | {task.Category} |  {task.Label} | Due: {task.DueDate}"
+                            );
+                        }
+                        command = AskForInput("Enter submit: ");
                     }
                 }
-                
-
-                if(viewEdit == "view tasks")
-                    {
-                    var result = Reporter.ShowTasksCompleted(dataManager.TaskData);
-                      Console.WriteLine("Your completed tasks are:");
-                      foreach (var task in result)
-{
-    Console.WriteLine($"- User: {task.User} | {task.Category} |  {task.Label} | Due: {task.DueDate}");
-}
-command = AskForInput("Enter submit: ");
-                    }
-            }
-            else if (selectedStatus == "upcoming")
-            {
-
-                var viewEdit = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                        .Title("Please select: view tasks or edit tasks:")
-                        .AddChoices("view tasks", "edit tasks"));
-                
-                if(viewEdit == "edit tasks") 
+                else if (selectedStatus == "upcoming")
                 {
-
-                var selectedCategory = AnsiConsole.Prompt(
-                    new SelectionPrompt<Category>()
-                        .Title("Please select task category: ")
-                        .AddChoices(dataManager.Categories));
-
-                var selectedLabel = AnsiConsole.Prompt(
-                    new SelectionPrompt<Label>()
-                        .Title("Please select task label: ")
-                        .AddChoices(selectedCategory.Labels));
-
-                var dueDate = AnsiConsole.Prompt(
-                    new TextPrompt<DateTime>("Please enter upcoming task due date and time (in m/d/y hr:min format):"));
-
-
-                TaskData data = new TaskData(
-                    dueDate, selectedUser, selectedCategory, selectedLabel, taskStatus);
-        
-                dataManager.AddNewTaskData(data);
-
-                command = AskForInput("Enter submit: ");
-            }
-    
-            if(viewEdit == "view tasks")
+                    var viewEdit = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Please select: view tasks or edit tasks:")
+                            .AddChoices("view tasks", "edit tasks")
+                    );
+                    if (viewEdit == "edit tasks")
                     {
-                      var result = Reporter.ShowTasksUpcoming(dataManager.TaskData);
-                      Console.WriteLine("Your upcoming tasks are:");
-                      foreach (var task in result)
-            {
-                Console.WriteLine($"- User: {task.User} | {task.Category} |  {task.Label} | Due: {task.DueDate}");
-            }
-                command = AskForInput("Enter submit: ");
-                    } 
+                        var updateEdit = AnsiConsole.Prompt(
+                            new SelectionPrompt<string>()
+                                .Title("Mark previously entered task as complete or add new task ")
+                                .AddChoices("update previously entered task", "add new task")
+                        );
+                        var selectedCategory = AnsiConsole.Prompt(
+                            new SelectionPrompt<Category>()
+                                .Title("Please select task category: ")
+                                .AddChoices(dataManager.Categories)
+                        );
 
-            }
-                } while (command != "submit");
+                        var selectedLabel = AnsiConsole.Prompt(
+                            new SelectionPrompt<Label>()
+                                .Title("Please select task label: ")
+                                .AddChoices(selectedCategory.Labels)
+                        );
+
+                        var dueDate = AnsiConsole.Prompt(
+                            new TextPrompt<DateTime>(
+                                "Please enter upcoming task due date and time (in m/d/y hr:min format):"
+                            )
+                        );
+
+                        TaskData data = new TaskData(
+                            dueDate,
+                            selectedUser,
+                            selectedCategory,
+                            selectedLabel,
+                            taskStatus
+                        );
+
+                        dataManager.AddNewTaskData(data);
+
+                        command = AskForInput("Enter submit: ");
+                    }
+
+                    if (viewEdit == "view tasks")
+                    {
+                        var result = Reporter.ShowTasksUpcoming(dataManager.TaskData);
+                        Console.WriteLine("Your upcoming tasks are:");
+                        foreach (var task in result)
+                        {
+                            Console.WriteLine(
+                                $"- User: {task.User} | {task.Category} |  {task.Label} | Due: {task.DueDate}"
+                            );
+                        }
+                        command = AskForInput("Enter submit: ");
+                    }
+                }
+            } while (command != "submit");
+        }
+        else if (user == "new user")
+        {
+            var newUserName = AnsiConsole.Prompt(new TextPrompt<string>("Enter new user's name:"));
+            dataManager.AddUser(new User(newUserName));
         }
     }
-        public static string AskForInput(string message){
-            Console.Write(message);
-            return Console.ReadLine() ?? "";
-        }
-        
+
+    public static string AskForInput(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine() ?? "";
     }
+}
--- 690Project/Project/DataManager.cs
+++ 690Project/Project/DataManager.cs
@@ -6,99 +6,126 @@ public class DataManager
 {
     FileSaver fileSaver;
     public List<Category> Categories { get; }
-    public List<Label> Labels { get; } 
+    public List<Label> Labels { get; }
     public List<User> Users { get; }
     public List<TaskData> TaskData { get; }
+
     public DataManager()
     {
-         fileSaver = new FileSaver("task-data.txt");
+        fileSaver = new FileSaver("task-data.txt");
 
-            Categories = new List<Category>(); 
-            var categoriesFileContent = File.ReadAllLines("categories.txt");
-
-            foreach(var categoryName in categoriesFileContent){
-                Categories.Add(new Category(categoryName));
-            }
-
-            Labels = new List<Label>();
-            var labelsFileContent = File.ReadAllLines("labels.txt");
-
-            foreach(var labelName in labelsFileContent){
-                Labels.Add(new Label(labelName));
+        Categories = new List<Category>();
+        var categoriesFileContent = File.ReadAllLines("categories.txt");
+        foreach (var categoryName in categoriesFileContent)
+        {
+            Categories.Add(new Category(categoryName));
         }
 
-            // add different Labels to different Categories 
-            Categories[0].Labels.Add(Labels[0]);
-            Categories[0].Labels.Add(Labels[1]);
-            Categories[1].Labels.Add(Labels[2]);
-            Categories[1].Labels.Add(Labels[3]);
-            Categories[2].Labels.Add(Labels[4]);
-            Categories[2].Labels.Add(Labels[5]);
+        Labels = new List<Label>();
+        var labelsFileContent = File.ReadAllLines("labels.txt");
+        foreach (var line in labelsFileContent)
+        {
+            var parts = line.Split('|');
+            var labelName = parts[0];
+            var categoryName = parts[1];
 
-            Users = new List<User>();
-            Users.Add(new User("Jane"));
-            Users.Add(new User("Ben"));
+            var category = Categories.First(c => c.Name == categoryName);
+            var label = new Label(labelName);
+            Labels.Add(label);
+            category.Labels.Add(label);
+        }
 
-            TaskData = new List<TaskData>();
+        Users = new List<User>();
+        var userFileContent = File.ReadAllLines("users.txt");
+        foreach (var userName in userFileContent)
+        {
+            Users.Add(new User(userName));
+        }
 
+        TaskData = new List<TaskData>();
         if (File.Exists("task-data.txt"))
         {
             var taskFileContent = File.ReadAllLines("task-data.txt");
-            foreach(var line in taskFileContent)
+            foreach (var line in taskFileContent)
             {
-                var splitted = line.Split("-", StringSplitOptions.RemoveEmptyEntries);
-                var userName = splitted[0];
-                var user = new User(userName);
-
-                var categoryName = splitted[1];
-                var category = new Category(categoryName);
-
-                var taskName = splitted[2];
-                var label = new Label(taskName);
-
-                var dueDate = DateTime.Parse(splitted[3]);
-
-                bool Complete = splitted[4].Trim().ToLower() == "complete";
-                Status status = new Status(Complete);
-           
+                var parts = line.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                var user = Users.First(u => u.Name == parts[0]);
+                var category = Categories.First(c => c.Name == parts[1]);
+                var label = category.Labels.First(l => l.Name == parts[2]);
+                var dueDate = DateTime.Parse(parts[3]);
+                var status = new Status(parts[4].Trim().ToLower() == "complete");
 
                 TaskData.Add(new TaskData(dueDate, user, category, label, status));
-            }; 
+            }
         }
-        
     }
+
+    public void SaveAllTasks()
+    {
+        File.Delete("task-data.txt");
+
+        foreach (var task in TaskData)
+        {
+            fileSaver.AppendData(task);
+        }
+    }
+
     public void AddNewTaskData(TaskData data)
     {
         this.TaskData.Add(data);
         this.fileSaver.AppendData(data);
     }
 
+    public void SynchronizeUsers()
+    {
+        File.Delete("users.txt");
+        foreach (var user in Users)
+        {
+            File.AppendAllText("users.txt", user.Name + Environment.NewLine);
+        }
+    }
+
+    public void AddUser(User user)
+    {
+        Users.Add(user);
+        SynchronizeUsers();
+    }
+
     public void SynchronizeLabels()
     {
         File.Delete("labels.txt");
-        foreach(var label in Labels)
+        foreach (var category in Categories)
         {
-            File.AppendAllText("labels.txt", label.Name+Environment.NewLine);
+            foreach (var label in category.Labels)
+            {
+                File.AppendAllText(
+                    "labels.txt",
+                    $"{label.Name}|{category.Name}{Environment.NewLine}"
+                );
+            }
         }
     }
 
-     public void SynchronizeCategories()
+    public void AddLabel(Label label, Category category)
+    {
+        Labels.Add(label);
+        category.Labels.Add(label);
+        SynchronizeLabels();
+        SynchronizeCategories();
+    }
+
+    public void SynchronizeCategories()
     {
         File.Delete("categories.txt");
-        foreach(var category in Categories)
+        foreach (var category in Categories)
         {
-            File.AppendAllText("categories.txt", category.Name+Environment.NewLine);
+            File.AppendAllText("categories.txt", category.Name + Environment.NewLine);
         }
     }
 
-
-public void SaveAllTasks()
-{
-    File.Delete("task-data.txt");
-
-    foreach (var task in TaskData)
+    public void AddCategory(Category category)
     {
-        fileSaver.AppendData(task);
+        Categories.Add(category);
+        SynchronizeCategories();
     }
-}
 }
