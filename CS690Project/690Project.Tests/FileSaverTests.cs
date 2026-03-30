6c54f46a370cd04fcd0f5192fef36f19aa7fa870
Sabrina <sabrinalk22@gmail.com>
2026-03-30 20:46:30 +0000

    chore: first test (file saver), and rename project

---
+++ CS690Project/690Project.Tests/FileSaverTests.cs
@@ -0,0 +1,22 @@
+﻿namespace CS690Project.Tests;
+
+using Project;
+public class FileSaverTests
+{
+    FileSaver fileSaver;
+    string testFileName;
+
+    public FileSaverTests()
+    {
+        testFileName = "test-doc.txt";
+        fileSaver = new FileSaver(testFileName);
+
+    }
+    [Fact]
+    public void Test_FileSave_Append()
+    {
+        fileSaver.AppendLine("Hello test user");
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal("Hello test user" + Environment.NewLine, contentFromFile);
+    }
+}
